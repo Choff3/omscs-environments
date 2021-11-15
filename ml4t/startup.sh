7eb033feb694f2120847a6278222820624c997ce
Choff3 <42592276+Choff3@users.noreply.github.com>
2021-11-15 16:31:38 -0500

    Updated script to have the option to leave out git

--- ml4t/startup.sh
+++ ml4t/startup.sh
@@ -4,7 +4,12 @@ CONTAINER_FIRST_STARTUP="CONTAINER_FIRST_STARTUP"
 if [ ! -e /$CONTAINER_FIRST_STARTUP ]; then
 	touch /$CONTAINER_FIRST_STARTUP
 	/root/miniconda3/bin/conda init bash
-	git clone https://${gitusername}:${gitpassword}@github.gatech.edu/${gitusername}/${gitrepo}.git
-	echo "cd /${gitrepo}" >> /root/.bashrc
-	echo "conda activate ml4t" >> /root/.bashrc
+	if ["${gitusername}" -ne ""]
+		git clone https://${gitusername}:${gitpassword}@github.gatech.edu/${gitusername}/${gitrepo}.git /root/${gitrepo}
+		echo "[user]" >> /root/.gitconfig
+		echo "name = ${gitusername}" >> /root/.gitconfig
+		echo "email = ${gitusername}@gatech.edu" >> /root/.gitconfig
+		echo "password = ${gitpassword}@gatech.edu" >> /root/.gitconfig
+		echo "cd /root/${gitrepo}" >> /root/.bashrc
+	fi
 fi
