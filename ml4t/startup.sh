25fdda01d1796e6888365e0281dbb720f94381ef
Choff3 <42592276+Choff3@users.noreply.github.com>
2021-11-15 17:08:09 -0500

    Fixed script

--- ml4t/startup.sh
+++ ml4t/startup.sh
@@ -4,12 +4,8 @@ CONTAINER_FIRST_STARTUP="CONTAINER_FIRST_STARTUP"
 if [ ! -e /$CONTAINER_FIRST_STARTUP ]; then
 	touch /$CONTAINER_FIRST_STARTUP
 	/root/miniconda3/bin/conda init bash
-	if ["${gitusername}" -ne ""]
-		git clone https://${gitusername}:${gitpassword}@github.gatech.edu/${gitusername}/${gitrepo}.git /root/${gitrepo}
-		echo "[user]" >> /root/.gitconfig
-		echo "name = ${gitusername}" >> /root/.gitconfig
-		echo "email = ${gitusername}@gatech.edu" >> /root/.gitconfig
-		echo "password = ${gitpassword}@gatech.edu" >> /root/.gitconfig
-		echo "cd /root/${gitrepo}" >> /root/.bashrc
-	fi
+	git clone https://${gitusername}:${gitpassword}@github.gatech.edu/${gitusername}/${gitrepo}.git /root/${gitrepo}
+	echo "https://${gitusername}:${gitpassword}@github.gatech.edu/${gitusername}/${gitrepo}.git" >> /root/.git-credentials
+	echo "cd /root/${gitrepo}" >> /root/.bashrc
+	echo "conda activate ml4t" >> /root/.bashrc
 fi
